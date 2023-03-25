fda0e3f083d3eb49a9a0d508094e979f1b96c2fa
Diego Salvatierra <70663170+salvatierrad@users.noreply.github.com>
2023-03-25 11:16:20 -0500

    Create python.py

---
+++ Software/python.py
@@ -0,0 +1,115 @@
+# Lenguajes de programación
+Dutante el curso se utilizará  Python.
+
+## Python
+
+```python
+"""
+Se usará Python porque es un lenguaje de programación popular y ampliamente utilizado en la industria debido a sus numerosas ventajas, que incluyen:
+- Amplia gama de bibliotecas y frameworks: Python tiene una gran cantidad de bibliotecas y frameworks para diversas tareas de programación, como ciencia de datos, aprendizaje automático, desarrollo web, etc. Esto hace que sea fácil para los programadores implementar soluciones en sus proyectos sin tener que escribir todo el código desde cero.
+- Sintaxis legible y fácil de aprender: Python se basa en una sintaxis legible que se asemeja mucho al lenguaje humano. Esto lo hace fácil de aprender para los principiantes y reduce la curva de aprendizaje.
+- Comunidad activa: Python tiene una gran comunidad de programadores activa y solidaria que contribuyen constantemente a proyectos de código abierto y ayudan a resolver problemas en foros en línea y sitios web de preguntas y respuestas.
+
+Además es muy sencillo programar con python, por ejemplo, el clásico:
+"""
+print("Hola mundo")
+
+equipo=["Diego","Mateo","Alessandra","Rosmy","Tayel","Jose"]
+for i in equipo:
+  print("Yo, ",equipo(i) + ", me comprometo a dar todo de mi para desarrollar un buen proyecto")
+
+a = 1
+b = 2
+c = 3
+vec = [a,b,c]
+for i in vec:
+  if i = 1:
+    print(vec(1))
+  else:
+    print(vec(i)+vec(i-1))
+
+equipo2 = ["Diego","Mateo","Alessandra","Rosmy","Tayel","Jose"]
+while i < length(equipo2):
+  print(equipo(i) + " forma parte del equipo E10 del curso Introducción a Señales Biomédicas")
+
+print("Hola mundo")
+
+equipo=["Diego","Mateo","Alessandra","Rosmy","Tayel","Jose"]
+for i in equipo:
+  print("Yo, ",equipo(i) + ", me comprometo a dar todo de mi para desarrollar un buen proyecto")
+
+a = 1
+b = 2
+c = 3
+vec = [a,b,c]
+for i in vec:
+  if i = 1:
+    print(vec(1))
+  else:
+    print(vec(i)+vec(i-1))
+
+equipo2 = ["Diego","Mateo","Alessandra","Rosmy","Tayel","Jose"]
+while i < length(equipo2):
+  print(equipo(i) + " forma parte del equipo E10 del curso Introducción a Señales Biomédicas")
+
+print("Hola mundo")
+
+equipo=["Diego","Mateo","Alessandra","Rosmy","Tayel","Jose"]
+for i in equipo:
+  print("Yo, ",equipo(i) + ", me comprometo a dar todo de mi para desarrollar un buen proyecto")
+
+a = 1
+b = 2
+c = 3
+vec = [a,b,c]
+for i in vec:
+  if i = 1:
+    print(vec(1))
+  else:
+    print(vec(i)+vec(i-1))
+
+equipo2 = ["Diego","Mateo","Alessandra","Rosmy","Tayel","Jose"]
+while i < length(equipo2):
+  print(equipo(i) + " forma parte del equipo E10 del curso Introducción a Señales Biomédicas")
+
+print("Hola mundo")
+
+equipo=["Diego","Mateo","Alessandra","Rosmy","Tayel","Jose"]
+for i in equipo:
+  print("Yo, ",equipo(i) + ", me comprometo a dar todo de mi para desarrollar un buen proyecto")
+
+a = 1
+b = 2
+c = 3
+vec = [a,b,c]
+for i in vec:
+  if i = 1:
+    print(vec(1))
+  else:
+    print(vec(i)+vec(i-1))
+
+equipo2 = ["Diego","Mateo","Alessandra","Rosmy","Tayel","Jose"]
+while i < length(equipo2):
+  print(equipo(i) + " forma parte del equipo E10 del curso Introducción a Señales Biomédicas")
+
+print("Hola mundo")
+
+equipo=["Diego","Mateo","Alessandra","Rosmy","Tayel","Jose"]
+for i in equipo:
+  print("Yo, ",equipo(i) + ", me comprometo a dar todo de mi para desarrollar un buen proyecto")
+
+a = 1
+b = 2
+c = 3
+vec = [a,b,c]
+for i in vec:
+  if i = 1:
+    print(vec(1))
+  else:
+    print(vec(i)+vec(i-1))
+
+equipo2 = ["Diego","Mateo","Alessandra","Rosmy","Tayel","Jose"]
+while i < length(equipo2):
+  print(equipo(i) + " forma parte del equipo E10 del curso Introducción a Señales Biomédicas")
+
+```
